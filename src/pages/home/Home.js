4e2b5ee5250697c4a1874927bff386fe4e0e5b3b
Remigijus66 <remigijus.bartaska@gmail.com>
2024-05-17 15:40:38 +0300

    png

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,12 +4,23 @@ import CircleWithButtons from "./components/CircleWithButtons";
 
 const Home = () => {
 
-  const mainMenu = [{ hour: 1, text: 'box text1' }, { hour: 2, text: 'box  longer text2' }, { hour: 3, text: 'box text3' }, { hour: 4, text: 'box longer text4' }, { hour: 5, text: 'box text5' }, { hour: 7, text: 'box text7' }, { hour: 8, text: 'box text8' }, { hour: 9, text: 'box longer text9' }, { hour: 10, text: 'box text10' }, { hour: 11, text: 'box text11' }]
+  const mainMenu = [
+    { hour: 1, text: 'box text1' },
+    { hour: 2, text: 'box  longer text2' }, 
+    { hour: 3, text: 'box text3' },
+    { hour: 4, text: 'box longer text4' }, 
+    { hour: 5, text: 'box text5' }, 
+    { hour: 7, text: 'box text7' }, 
+    { hour: 8, text: 'box text8' }, 
+    { hour: 9, text: 'box longer text9' }, 
+    { hour: 10, text: 'box text10' }, 
+    { hour: 11, text: 'box text11' }]
+  const centerText = 'Apskaitos sprendimai visiems'
 
   return (
-    <>
-      <CircleWithButtons buttons={mainMenu} size={120} color={'green'}/>
-    </>
+    <div className='container blended-background'>
+      {/* <CircleWithButtons buttons={mainMenu} text={centerText} size={120} color={'green'}/> */}
+    </div>
 
   );
 };
--- src/pages/home/components/CircleWithButtons.js
+++ src/pages/home/components/CircleWithButtons.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const CircleWithButtons = ({buttons, size, color}) => {
+const CircleWithButtons = ({ buttons, text, size, color }) => {
   const nav = useNavigate()
 
   const canvasRef = useRef(null);
 
-  const navigate = (number) =>{
+  const navigate = (number) => {
     nav(`/menu-item/${number}`)
   }
   const wrapText = (context, text, x, y, maxWidth, lineHeight) => {
@@ -16,38 +16,38 @@ const CircleWithButtons = ({buttons, size, color}) => {
     let testWidth = 0;
 
     for (let i = 0; i < words.length; i++) {
-        testLine = line + words[i] + ' ';
-        testWidth = context.measureText(testLine).width;
-        if (testWidth > maxWidth && i > 0) {
-            context.fillText(line, x, y);
-            line = words[i] + ' ';
-            y += lineHeight;
-        } else {
-            line = testLine;
-        }
-    }
-    context.fillText(line, x, y);
-}
-const  calculateTextHeight = (context, text, maxWidth, lineHeight) => {
-  const words = text.split(' ');
-  let line = '';
-  let testLine = '';
-  let testWidth = 0;
-  let height = 0;
-
-  for (let i = 0; i < words.length; i++) {
       testLine = line + words[i] + ' ';
       testWidth = context.measureText(testLine).width;
       if (testWidth > maxWidth && i > 0) {
-          height += lineHeight;
-          line = words[i] + ' ';
+        context.fillText(line, x, y);
+        line = words[i] + ' ';
+        y += lineHeight;
       } else {
-          line = testLine;
+        line = testLine;
       }
+    }
+    context.fillText(line, x, y);
   }
-  height += lineHeight; 
-  return height;
-}
+  const calculateTextHeight = (context, text, maxWidth, lineHeight) => {
+    const words = text.split(' ');
+    let line = '';
+    let testLine = '';
+    let testWidth = 0;
+    let height = 0;
+
+    for (let i = 0; i < words.length; i++) {
+      testLine = line + words[i] + ' ';
+      testWidth = context.measureText(testLine).width;
+      if (testWidth > maxWidth && i > 0) {
+        height += lineHeight;
+        line = words[i] + ' ';
+      } else {
+        line = testLine;
+      }
+    }
+    height += lineHeight;
+    return height;
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -56,37 +56,37 @@ const  calculateTextHeight = (context, text, maxWidth, lineHeight) => {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
-    const startAngle1 = Math.PI / 3 * 5; 
-    const endAngle1 = Math.PI / 3; 
-    const startAngle2 = Math.PI / 3 * 2; 
-    const endAngle2 = Math.PI / 3 * 4; 
-   
-   const drawSquare = (squareColor, squareX, squareY, squareSize) => {
+    const startAngle1 = Math.PI / 3 * 5;
+    const endAngle1 = Math.PI / 3;
+    const startAngle2 = Math.PI / 3 * 2;
+    const endAngle2 = Math.PI / 3 * 4;
+
+    const drawSquare = (squareColor, squareX, squareY, squareSize) => {
       ctx.fillStyle = squareColor;
       ctx.fillRect(squareX, squareY, squareSize, squareSize);
-  }
-  const drawArrowToLowerRight = (arrowColor, arrowCenterX, arrowCenterY, arrowSize ) => {
-    ctx.strokeStyle = arrowColor;
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(arrowCenterX - arrowSize / 2, arrowCenterY - arrowSize / 2); // Start point of the arrow
-    ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 2); // Main line of the arrow
-    ctx.moveTo(arrowCenterX + arrowSize / 4, arrowCenterY + arrowSize / 2); // Left side of arrowhead
-    ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 2); // Connect to main line
-    ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 4); // Right side of arrowhead
-    ctx.stroke();
-}
-const  drawArrowToLowerLeft = (arrowColor, arrowCenterX, arrowCenterY, arrowSize) => {
-  ctx.strokeStyle = arrowColor;
-  ctx.lineWidth = 3;
-  ctx.beginPath();
-  ctx.moveTo(arrowCenterX + arrowSize / 2, arrowCenterY - arrowSize / 2); // Start point of the arrow
-  ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 2); // Main line of the arrow
-  ctx.moveTo(arrowCenterX - arrowSize / 4, arrowCenterY + arrowSize / 2); // Right side of arrowhead
-  ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 2); // Connect to main line
-  ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 4); // Left side of arrowhead
-  ctx.stroke();
-}
+    }
+    const drawArrowToLowerRight = (arrowColor, arrowCenterX, arrowCenterY, arrowSize) => {
+      ctx.strokeStyle = arrowColor;
+      ctx.lineWidth = 3;
+      ctx.beginPath();
+      ctx.moveTo(arrowCenterX - arrowSize / 2, arrowCenterY - arrowSize / 2); // Start point of the arrow
+      ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 2); // Main line of the arrow
+      ctx.moveTo(arrowCenterX + arrowSize / 4, arrowCenterY + arrowSize / 2); // Left side of arrowhead
+      ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 2); // Connect to main line
+      ctx.lineTo(arrowCenterX + arrowSize / 2, arrowCenterY + arrowSize / 4); // Right side of arrowhead
+      ctx.stroke();
+    }
+    const drawArrowToLowerLeft = (arrowColor, arrowCenterX, arrowCenterY, arrowSize) => {
+      ctx.strokeStyle = arrowColor;
+      ctx.lineWidth = 3;
+      ctx.beginPath();
+      ctx.moveTo(arrowCenterX + arrowSize / 2, arrowCenterY - arrowSize / 2); // Start point of the arrow
+      ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 2); // Main line of the arrow
+      ctx.moveTo(arrowCenterX - arrowSize / 4, arrowCenterY + arrowSize / 2); // Right side of arrowhead
+      ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 2); // Connect to main line
+      ctx.lineTo(arrowCenterX - arrowSize / 2, arrowCenterY + arrowSize / 4); // Left side of arrowhead
+      ctx.stroke();
+    }
 
     // Draw the circle segments
     const radius = size;
@@ -113,14 +113,14 @@ const  drawArrowToLowerLeft = (arrowColor, arrowCenterX, arrowCenterY, arrowSize
 
     // Add text inside the inner circle
     ctx.fillStyle = 'white';
-    ctx.font = '16px Arial';
+    ctx.font = `${size / 6}px Arial`;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     // ctx.fillText('Apskaitos sprendimai visiems', innerX, innerY);
-     const correction = calculateTextHeight(ctx, 'Apskaitos sprendimai visiems',  innerRadius, 20)
-   wrapText(ctx, 'Apskaitos sprendimai visiems', innerX, innerY-correction/4, innerRadius, 20)
-   
-   
+    const correction = calculateTextHeight(ctx, text, innerRadius, size / 5)
+    wrapText(ctx, text, innerX, innerY - correction / 4, innerRadius, size / 5)
+
+
     // Calculate positions for the buttons
     // console.log(buttons)
     // const hours = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
@@ -140,7 +140,7 @@ const  drawArrowToLowerLeft = (arrowColor, arrowCenterX, arrowCenterY, arrowSize
       // Draw line from circle to button
       ctx.beginPath();
       ctx.moveTo(lineStartX, lineStartY);
-      ctx.lineTo(buttonX , buttonY);
+      ctx.lineTo(buttonX, buttonY);
       ctx.stroke();
 
       // Draw the button
@@ -149,38 +149,35 @@ const  drawArrowToLowerLeft = (arrowColor, arrowCenterX, arrowCenterY, arrowSize
       const buttonCorrectedX = buttonX + (buttonWidth / 3) * (Math.cos(angle) / Math.abs(Math.cos(angle)))
       ctx.strokeRect(buttonCorrectedX - buttonWidth / 2, buttonY - buttonHeight / 2, buttonWidth, buttonHeight);
       ctx.fillStyle = 'black';
-      ctx.font = '12px Arial';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      const correction = calculateTextHeight(ctx, button.text,  buttonWidth - buttonHeight, 12)
-      wrapText(ctx, button.text, (Math.cos(angle)>0 ?  buttonCorrectedX + buttonHeight/2 : buttonCorrectedX - buttonHeight/2), correction > 12 ? buttonY - correction/4 : buttonY, buttonWidth - buttonHeight, 12 )
+      ctx.font = `${size / 8}px Arial`;
+      const correction = calculateTextHeight(ctx, button.text, buttonWidth - buttonHeight, size / 8)
+      wrapText(ctx, button.text, (Math.cos(angle) > 0 ? buttonCorrectedX + buttonHeight / 2 : buttonCorrectedX - buttonHeight / 2), correction > size / 8 ? buttonY - correction / 4 : buttonY, buttonWidth - buttonHeight, size / 8)
 
-      drawSquare(color, (Math.cos(angle)>0 ?  buttonCorrectedX -buttonWidth/2 : buttonCorrectedX + buttonHeight/2), buttonY-buttonHeight/2, buttonHeight )
-      Math.cos(angle)>0 ? drawArrowToLowerRight('white', buttonCorrectedX - buttonHeight ,buttonY, buttonHeight/3 ) : drawArrowToLowerLeft('white', buttonCorrectedX + buttonHeight ,buttonY, buttonHeight/3 )
+      drawSquare(color, (Math.cos(angle) > 0 ? buttonCorrectedX - buttonWidth / 2 : buttonCorrectedX + buttonHeight / 2), buttonY - buttonHeight / 2, buttonHeight)
+      Math.cos(angle) > 0 ? drawArrowToLowerRight('white', buttonCorrectedX - buttonHeight, buttonY, buttonHeight / 3) : drawArrowToLowerLeft('white', buttonCorrectedX + buttonHeight, buttonY, buttonHeight / 3)
       ctx.strokeStyle = 'black';
       ctx.lineWidth = 2;
 
-  // Button click handler
-  canvas.addEventListener('click', function(event) {
-    const rect = canvas.getBoundingClientRect();
-    const clickX = event.clientX - rect.left;
-    const clickY = event.clientY - rect.top;
-    if (
-      clickX >= buttonX - buttonWidth / 2 &&
-      clickX <= buttonX + buttonWidth / 2 &&
-      clickY >= buttonY - buttonHeight / 2 &&
-      clickY <= buttonY + buttonHeight / 2
-    ) {
-      console.log(`Button ${button.hour} clicked!`);
-      // Call your function here
-      navigate(button.hour)
-    }
-  });
-      
+      // Button click handler
+      canvas.addEventListener('click', function (event) {
+        const rect = canvas.getBoundingClientRect();
+        const clickX = event.clientX - rect.left;
+        const clickY = event.clientY - rect.top;
+        if (
+          clickX >= buttonX - buttonWidth / 2 &&
+          clickX <= buttonX + buttonWidth / 2 &&
+          clickY >= buttonY - buttonHeight / 2 &&
+          clickY <= buttonY + buttonHeight / 2
+        ) {
+          // console.log(`Button ${button.hour} clicked!`);
+          navigate(button.hour)
+        }
+      });
+
     });
   }, []);
 
-  return <canvas ref={canvasRef} width={size*5} height={size*5} />;
+  return <canvas ref={canvasRef} width={size * 5} height={size * 5} />;
 };
 
 export default CircleWithButtons;
